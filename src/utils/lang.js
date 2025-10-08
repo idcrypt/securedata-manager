827396bd7c3b0bf4ca77935c5dbf95be888a5536
idcrypt <nagagini512@gmail.com>
2025-10-08 20:11:00 +0700

    Update lang.js

--- src/utils/lang.js
+++ src/utils/lang.js
@@ -4,7 +4,13 @@ export const LANGS = {
 };
 
 export async function loadLanguage(lang) {
-  const res = await fetch(LANGS[lang]);
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(LANGS[lang]);
+    if (!res.ok) throw new Error("Language file not found");
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error("Language load error:", err);
+    return {};
+  }
 }
